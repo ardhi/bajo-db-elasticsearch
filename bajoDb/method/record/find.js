b5fd28f2e40e47fcad05bfa23ac2527c6e44c566
Ardhi Lukianto <ardhi@lukianto.com>
2024-04-14 11:09:30 +0700

    Updates

--- bajoDb/method/record/find.js
+++ bajoDb/method/record/find.js
@@ -7,7 +7,6 @@ async function find ({ schema, filter = {}, options = {} } = {}) {
   const { map, forOwn, isEmpty, get, omit } = await importPkg('lodash-es')
   const { prepPagination } = this.bajoDb.helper
   const { limit, skip, query, sort, page } = await prepPagination(filter, schema)
-  const { dataOnly, noCount } = options
   const criteria = query ? convert(query) : undefined
   const sorts = []
   forOwn(sort, (v, k) => {
@@ -18,14 +17,14 @@ async function find ({ schema, filter = {}, options = {} } = {}) {
     index: schema.collName,
     from: skip,
     size: limit,
-    track_total_hits: !dataOnly,
+    track_total_hits: !options.dataOnly,
     sort: isEmpty(sorts) ? undefined : sorts.join(',')
   })
   const results = map(resp.hits.hits, '_source')
   let count = 0
-  if (!noCount && !dataOnly) count = get(resp.hits, 'total.value')
+  if (options.count && !options.dataOnly) count = get(resp.hits, 'total.value')
   let result = { data: results, page, limit, count, pages: Math.ceil(count / limit) }
-  if (noCount) result = omit(result, ['count', 'pages'])
+  if (!options.count) result = omit(result, ['count', 'pages'])
   return result
 }
 
