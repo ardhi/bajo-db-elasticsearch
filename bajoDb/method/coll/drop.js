8746af0a0c5aa30aef11b7a43d2ae6296b53b0ad
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-28 20:18:59 +0700

    Moved to dobo-elasticsearch

--- bajoDb/method/coll/drop.js
+++
@@ -1,7 +0,0 @@
-async function collDrop ({ schema, options = {} }) {
-  const { getInfo } = this.app.bajoDb
-  const { instance } = getInfo(schema)
-  await instance.client.indices.delete({ index: schema.collName })
-}
-
-export default collDrop
--- bajoDb/method/coll/exists.js
+++
@@ -1,8 +0,0 @@
-async function collExists ({ schema, options = {} }) {
-  const { getInfo } = this.app.bajoDb
-  const { instance } = getInfo(schema)
-  const exists = await instance.client.indices.exists({ index: schema.collName })
-  return !!exists
-}
-
-export default collExists
